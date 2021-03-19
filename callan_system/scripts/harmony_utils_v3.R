55766984336b4299e3bb272916c663819651b3a6
RobertSellers <robertwsellers@gmail.com>
2021-03-19 09:41:03 -0400

    alterations to sub system

--- callan_system/scripts/harmony_utils_v3.R
+++ callan_system/scripts/harmony_utils_v3.R
@@ -76,7 +76,7 @@ harmony_create_collection <- function(dir, overwrite = FALSE){
 
   # data cache handling
   # incomplete - need parameter for overwrite
-  temp_data_storage <- paste0("../temp_files/",dir,'.RData')
+  temp_data_storage <- paste0("./temp_files/",dir,'.RData')
 
     # look for r data
     if (file.exists(temp_data_storage) && overwrite == FALSE){
@@ -125,29 +125,33 @@ harmony_create_collection <- function(dir, overwrite = FALSE){
             }
           })
           
-          # ###### PLATE FILE#######
-          plate_file <- file.path(cur_path, paste0("Evaluation", e), "PlateResults.txt")
           
-          if(file.exists(plate_file)){
+          # ###### PLATE FILE#######
+          plate_df <- data.frame()
+          try({
+            plate_file <- file.path(cur_path, paste0("Evaluation", e), "PlateResults.txt")
             
-            # handle metadata for plate header
-            plate_header_raw <- readLines(plate_file,n = 8)
-            split_header <- strsplit(plate_header_raw, split="\t")
-            metadata_df <- as.data.frame(t(do.call(rbind,split_header[c(1:6)]))) %>%
-              row_to_names(row_number = 1)
-            
-            plate_df <- read_tsv(plate_file,skip=8) %>%
-              select_if(function(x) any(!is.na(x))) %>% # removes columns with all NA
-              janitor::clean_names() %>%# removes spaces and bad characters for underscores
-              mutate(dir = plate_obj_name)
-            plate_df[is.nan(plate_df)] <- NA #change NaN to NA
-            
-          }else{
-            stop('Plate data not loaded')
-          }
+            if(file.exists(plate_file)){
+              
+              # handle metadata for plate header
+              plate_header_raw <- readLines(plate_file,n = 8)
+              split_header <- strsplit(plate_header_raw, split="\t")
+              metadata_df <- as.data.frame(t(do.call(rbind,split_header[c(1:6)]))) %>%
+                row_to_names(row_number = 1)
+              
+              plate_df <- read_tsv(plate_file,skip=8) %>%
+                select_if(function(x) any(!is.na(x))) %>% # removes columns with all NA
+                janitor::clean_names() %>%# removes spaces and bad characters for underscores
+                mutate(dir = plate_obj_name)
+              plate_df[is.nan(plate_df)] <- NA #change NaN to NA
+              
+            }else{
+              warning('Plate data not loaded')
+            }
+          })
           
           # ###### CELLS FILE#######
-          #cells_file <- file.path(cur_path, paste0("Evaluation", e), "Objects_Population - Nuclei Selected.txt")
+          cell_df <- data.frame()
           cells_file <- list.files(file.path(cur_path, paste0("Evaluation", e)), full.names = TRUE, pattern = "Objects_Population")[1]
           if(file.exists(cells_file)){
             
